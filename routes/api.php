93c27fc303b795993a3f09b11215cc917d6e875e
Zachary <zacharylim2004@gmail.com>
2023-02-09 02:51:50 +0800

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Catch-all route
+Route::fallback(function () {
+    return response()->json([
+        "type" => "Not found",
+        "message" => "There's nothing here.."
+    ], 404);
 });
