bbe763d7e4bd8758fcdcfe40dbab6f89a34c2bad
Zachary <zacharylim2004@gmail.com>
2023-02-09 19:00:25 +0800

    Updated Service type feature tests

--- tests/Feature/Models/ServiceTypeTest.php
+++ tests/Feature/Models/ServiceTypeTest.php
@@ -2,21 +2,94 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\ServiceType;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ServiceTypeTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
-     * A basic feature test example.
+     * Feature test for getting all service types.
      *
      * @return void
      */
-    public function test_example()
+    public function test_get_all_service_types(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/types');
 
-        $response->assertStatus(404);
+        $response->assertOk()->assertJsonStructure([
+            'type',
+            'message',
+            'serviceTypes' => [
+                '*' => [
+                    'id',
+                    'label',
+                    'price',
+                    'available',
+                    'quantifiable',
+                    'created_at',
+                    'updated_at',
+                    'products' => [
+                        '*' => [
+                            'label',
+                            'quantity'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * Feature test for getting a service type.
+     *
+     * @return void
+     */
+    public function test_get_service_type(): void
+    {
+        $id = ServiceType::first()->id;
+
+        $response = $this->get("/types/$id");
+
+        $response->assertOk()->assertJsonStructure([
+            'type',
+            'message',
+            'serviceType' => [
+                'id',
+                'label',
+                'price',
+                'available',
+                'quantifiable',
+                'created_at',
+                'updated_at',
+                'products' => [
+                    '*' => [
+                        'label',
+                        'quantity'
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * Feature test for getting a service type that doesn't exist or is blocked.
+     *
+     * @return void
+     */
+    public function test_get_invalid_service_type(): void
+    {
+        $id = fake()->uuid();
+
+        $response = $this->get("/types/$id");
+
+        $response->assertNotFound()->assertJsonStructure([
+            'type',
+            'message'
+        ]);
     }
 }
